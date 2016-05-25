00b626cc3b9cb1a3d3a45cf91347fcfb115b9ec9
Tyler Martinez <slightlytyler@gmail.com>
2016-05-25 14:40:59 -0500

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Node Boilerplate
+
+## Development
+
+From a blank directory:
+
+```
+git clone https://github.phytel.com/tmartinez/node-boilerplate.git .
+npm install
+npm run dev
+```
+
+## Deployment
+
+```
+npm run deploy
+```
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const webpack = require('webpack');
+const argv = require('yargs').argv;
+
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+const __src = path.join(__dirname, 'src');
+const __dist = path.join(__dirname, 'dist');
+const __node_modules = path.join(__dirname, 'node_modules');
+
+const env = process.env.NODE_ENV || 'development';
+
+const globals = {
+  'process.env': {
+    'NODE_ENV': JSON.stringify(env)
+  },
+  NODE_ENV: env,
+  __DEV__: env === 'development',
+  __PROD__: env === 'production',
+  __TEST__: env === 'test',
+  __DEBUG__: env === 'development' && !argv.no_debug,
+  __DEBUG_NEW_WINDOW__: !!argv.nw,
+  __BASENAME__: JSON.stringify(process.env.BASENAME || '')
+}
+
+module.exports = {
+  devtool: 'eval',
+  entry: path.join(__src, 'index.js'),
+  target: 'node',
+  output: {
+    path: __dist,
+    filename: 'bundle.js'
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.DefinePlugin(globals)
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        include: __src,
+        loader: 'eslint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: __src,
+      }
+    ]
+  },
+  resolve: {
+    fallback: __node_modules,
+    alias: {
+      src: __src
+    }
+  },
+  resolveLoader: { fallback: __node_modules }
+};
