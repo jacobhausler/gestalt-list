ee241dda039ebfa6e6a2380caeead880147a4db9
Jacob Hausler <jacob@hausler.io>
2016-07-14 22:23:31 -0500

    added ViewMessage mutation and asserts on update calls

--- src/modules/message/mutations.js
+++ src/modules/message/mutations.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import { stripId } from 'helpers/data';
+import { isNil } from 'lodash';
 
 export const Send = types => ({
   name: 'SendMessage',
@@ -24,14 +25,52 @@ export const Send = types => ({
       { id: stripId(conversationId) },
       { updatedAt: new Date() },
     );
+    assert(currentConversation, "Conversation doesn't exist!");
+
     // fails if the db call fails
     await db.insert('messages', {
       createdAt: new Date(),
       authoredByUserId: currentUserId,
       heldByConversationId: stripId(conversationId),
+      seen: false,
       body,
     });
 
     return { currentConversation };
+  },
+});
+
+export const View = types => ({
+  name: 'ViewMessage',
+  inputFields: {
+    messageId: types.ID,
+  },
+  outputFields: {
+    changedConversation: types.Conversation,
+  },
+  mutateAndGetPayload: async (
+    { messageId },
+    { db }
+  ) => {
+    // update the message
+    const updatedMessage = await db.update(
+      'messages',
+      { id: stripId(messageId) },
+      { seen: true },
+    );
+    assert(!isNil(updatedMessage), "Message doesn't exist!");
+
+    // update the currentConversation
+    const conversation = await db.update(
+      'conversations',
+      { id: updatedMessage[0].heldByConversationId },
+      { updatedAt: new Date() },
+    );
+    assert(!isNil(conversation), "Conversation doesn't exist!");
+    console.log(conversation);
+
+    const changedConversation = conversation[0];
+
+    return { changedConversation };
   },
 });
